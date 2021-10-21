9dd684c78fd84c999488f810cc6635bc9f3a5a2c
Arunava-Barua <rahulbarua31@gmail.com>
2021-10-21 14:29:41 +0530

    Added the search and filtering feature

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import React, { Component } from "react";
 import "./App.css";
-import {CardList} from './components/card-list/card-list.component.jsx'
+import { CardList } from "./components/card-list/card-list.component.jsx";
 // import { render } from "@testing-library/react";
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
 
     this.state = {
-      monster: [],
+      monsters: [],
+      searchField: "",
     };
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then(users => this.setState({monster: users}))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
-  render () {
+  render() { 
+    const { monsters, searchField } = this.state; 
+    
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    // console.log(filteredMonsters);
+    
     return (
       <div className="App">
-        <CardList monsters = {this.state.monster}>
-          
-        </CardList>
+        <input
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(e) => {
+            // console.log(this);
+            this.setState({ searchField: e.target.value });
+          }}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
-  } 
+  }
 }
 
 export default App;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Card } from "../card/card.component";
-import './card-list.styles.css'
+import "./card-list.styles.css";
 
 export const CardList = function (props) {
-    // console.log(props);
-    return (
-      <div className="card-list">
-        {props.monsters.map((monster) => (
-          <Card key={monster.id} monster = {monster}> </Card>
-        ))}
-      </div>
-    );
-}
+  // console.log(props);
+  return (
+    <section className="card-list">
+      {props.monsters.map((monster) => {
+        // console.log(monster);
+        return <Card key={monster.id} monster={monster} />;
+      })}
+    </section>
+  );
+};
